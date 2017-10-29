70560f3e5c07031ed935c87f18f515d62ca49aee
Catherine Koshchenko <catherine.pths@gmail.com>
2017-10-29 15:54:55 +0300

    Added test

--- src/test/java/ru/spbau/mit/test.kt
+++ src/test/java/ru/spbau/mit/test.kt
@@ -1,10 +1,15 @@
 package ru.spbau.mit
+
 import kotlin.test.assertEquals
 import org.junit.Test
 
 class TestSource {
     @Test
-    fun testGreeting() {
-        assertEquals("Hello, world!", getGreeting())
+    fun sampleTests() {
+        assertEquals("IMPOSSIBLE", PatternFiller(3, "a?c").solve())
+        assertEquals("abba", PatternFiller(2, "a??a").solve())
+        assertEquals("abba", PatternFiller(2, "?b?a").solve())
+        assertEquals("aaa", PatternFiller(1, "a?a").solve())
+        assertEquals("aabaa", PatternFiller(2, "a???a").solve())
     }
 }
